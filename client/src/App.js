243c3db6431e335c337e3d82b6c34c9c4ebef42c
sandeepa <sandeepamallawarachchi444@gmail.com>
2024-09-19 00:05:18 +0530

    dashboard fixed

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
-import React from "react";
-import "./index.css"
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import './index.css';
+import Sidebar from './components/Sidebar';
 
-import Header from "./components/Header";
-import Dashboard from "./pages/Dashboard";
-
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-      </Routes>
+      <div className="flex h-screen">
+        <Sidebar />
+        <div className="flex-1 flex flex-col">
+          <Header />
+          <div className="flex-1 p-4 overflow-auto">
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              {/* Add more routes here as needed */}
+            </Routes>
+          </div>
+        </div>
+      </div>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -3,36 +3,31 @@ import avatar from '../images/avatar.png';
 import Sidebar from './Sidebar';
 
 const Header = () => {
-    return (
-        <div className="flex">
-            <Sidebar />
-            <div className="flex-1">
-                <header className="w-full h-auto px-4 py-2 bg-gradient-to-r from-[#fa7c10] to-[#ffad33] flex items-center justify-between">
-                    <div className="text-white ml-5">
-                        Dashboard
-                    </div>
-                    <div className="relative flex items-center border border-orange-500 rounded-full p-2 px-3 space-x-3 backdrop-blur-lg group">
-                        <img
-                            src={avatar}
-                            alt="avatar"
-                            className="w-10 h-10 rounded-full border border-gray-300"
-                        />
-                        <span className="text-white cursor-pointer">
-                            Sandeepa Mallawarachchi
-                        </span>
-                        <div className="absolute right-0 top-full mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-md">
-                            <ul className="py-2">
-                                <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Profile</li>
-                                <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Change Password</li>
-                                <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Logout</li>
-                                <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Support</li>
-                            </ul>
-                        </div>
-                    </div>
-                </header>
-            </div>
+  return (
+    <div className="header w-full h-auto px-4 py-2 bg-gradient-to-r from-[#fa7c10] to-[#ffad33] flex items-center justify-between">
+      <div className="text-white ml-5">
+        Dashboard
+      </div>
+      <div className="relative flex items-center border border-orange-500 rounded-full p-2 px-3 space-x-3 backdrop-blur-lg group">
+        <img
+          src={avatar}
+          alt="avatar"
+          className="w-10 h-10 rounded-full border border-gray-300"
+        />
+        <span className="text-white cursor-pointer">
+          Sandeepa Mallawarachchi
+        </span>
+        <div className="absolute right-0 top-full mt-2 w-48 bg-white border border-gray-200 rounded-lg shadow-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 backdrop-blur-md">
+          <ul className="py-2">
+            <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Profile</li>
+            <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Change Password</li>
+            <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Logout</li>
+            <li className="px-4 py-2 hover:bg-orange-100 cursor-pointer">Support</li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Header;
