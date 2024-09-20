06af83b51a98384549ee98b666cdc47da8d3b28d
sandeepa <sandeepamallawarachchi444@gmail.com>
2024-09-20 18:00:17 +0530

    leave and payroll pages

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,22 @@ import Header from './components/Header';
 import Dashboard from './pages/Dashboard';
 import Sidebar from './components/Sidebar';
 import Profile from './components/Profile';
+import Leave from './pages/Leave';
+import Payroll from './pages/Payroll';
 
 const App = () => {
   return (
     <BrowserRouter>
       <div className="flex h-screen">
         <Sidebar />
-        <div className="flex-1 flex flex-col ml-[-40px]">
+        <div className="flex-1 flex flex-col ml-[-35px]">
           <Header />
           <div className="flex-1 p-4 pl-10 overflow-auto bg-[#f6f5fb]">
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/profile" element={<Profile />} />
+              <Route path="/leave" element={<Leave />} />
+              <Route path="/payroll" element={<Payroll />} />
             </Routes>
           </div>
         </div>
