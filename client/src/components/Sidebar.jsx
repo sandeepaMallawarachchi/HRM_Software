ff1b67725ab7eceaf0b2da1e044d02bc1facf083
thisaldil <tdimith10@gmail.com>
2024-10-25 16:19:39 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,25 +10,27 @@ import {
   FaCheckDouble,
   FaGripfire,
   FaAngleRight,
-  FaTachometerAlt, // For Executive Dashboard Icon
+  FaFire,
 } from "react-icons/fa";
-
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import logo from "../images/hrm withoutbackground.png";
 import TeamLeaderSidebar from "../components/SidebarComponents/TeamLdrSidebar";
-import MidLvlManagersidebar from "../components/SidebarComponents/MidLvlManagersidebar";
-import TopLvlManagersidebar from "../components/SidebarComponents/TopLvlManagersidebar";
-import CeoSidebar from "../components/SidebarComponents/CeoSidebar";
-import HRsidebar from "./SidebarComponents/HRsidebar";
 
-const Sidebar = () => {
+const Sidebar = ({ role }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
-  const [isRecruitmentOpen, setIsRecruitmentOpen] = useState(false); // For toggling Recruitment submenu
-  const role = localStorage.getItem("role");
+  const [isRecruitmentOpen, setIsRecruitmentOpen] = useState(false);
 
   const handleToggle = () => {
     setIsCollapsed(!isCollapsed);
   };
+
+  // Use useLocation hook to get the current route path
+  const location = useLocation();
+
+  // Check if either "offers" or "onboarding" is the current active route
+  const isRecruitmentActive =
+    location.pathname.includes("/offers") ||
+    location.pathname.includes("/onboarding");
 
   return (
     <div
@@ -36,7 +38,6 @@ const Sidebar = () => {
         isCollapsed ? "w-16" : "w-64"
       } pr-3 h-screen bg-white text-black shadow-xl transition-all duration-300 rounded-r-[40px] border-r relative`}
     >
-      {/* Toggle Button */}
       <button
         onClick={handleToggle}
         className="absolute top-20 right-[-16px] bg-gradient-to-r from-orange-400 to-orange-500 text-white p-2 rounded-full shadow-md hover:bg-orange-400"
@@ -44,7 +45,6 @@ const Sidebar = () => {
         {isCollapsed ? <FaAngleRight size={20} /> : <FaAngleLeft size={20} />}
       </button>
 
-      {/* Logo and HRM Section */}
       <div className="flex flex-col items-center mt-5 w-full">
         <Link to="/dashboard" className="flex flex-col items-center">
           <img
@@ -62,47 +62,24 @@ const Sidebar = () => {
         </Link>
       </div>
 
-      {/* Navigation Links */}
-      <div className="flex flex-col items-center justify-end mt-4 mb-8 w-full gap-2">
-        {/* Executive Dashboard (only for CEO) */}
-        {role === "Ceo" ? (
-          <NavLink
-            to="/executive-dashboard"
-            className={({ isActive }) =>
-              `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
-                isActive
-                  ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
-                  : ""
-              }`
-            }
-          >
-            <FaTachometerAlt
-              size={20}
-              className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
-            />
-            {!isCollapsed && <span>Executive Dashboard</span>}
-          </NavLink>
-        ) : (
-          /* Dashboard Link (hide for CEO) */
-          <NavLink
-            to="/dashboard"
-            className={({ isActive }) =>
-              `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
-                isActive
-                  ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
-                  : ""
-              }`
-            }
-          >
-            <FaHome
-              size={20}
-              className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
-            />
-            {!isCollapsed && <span>Dashboard</span>}
-          </NavLink>
-        )}
+      <div className="flex flex-col items-center justify-end mt-4 mb-8 w-full gap-5">
+        <NavLink
+          to="/dashboard"
+          className={({ isActive }) =>
+            `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
+              isActive
+                ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
+                : ""
+            }`
+          }
+        >
+          <FaHome
+            size={20}
+            className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
+          />
+          {!isCollapsed && <span>Dashboard</span>}
+        </NavLink>
 
-        {/* Payroll Link */}
         <NavLink
           to="/payroll"
           className={({ isActive }) =>
@@ -120,7 +97,6 @@ const Sidebar = () => {
           {!isCollapsed && <span>Payroll</span>}
         </NavLink>
 
-        {/* Attendance & Time Link */}
         <NavLink
           to="/attendance"
           className={({ isActive }) =>
@@ -138,7 +114,6 @@ const Sidebar = () => {
           {!isCollapsed && <span>Attendance & Time</span>}
         </NavLink>
 
-        {/* Leave & Attendance Link */}
         <NavLink
           to="/leave"
           className={({ isActive }) =>
@@ -156,12 +131,45 @@ const Sidebar = () => {
           {!isCollapsed && <span>Leave & Attendance</span>}
         </NavLink>
 
-        {/* HR Role-Specific Links */}
-        {role === "HR" && (
-          <>
-            {/* Learning & Development */}
+        <NavLink
+          to="/learn"
+          className={({ isActive }) =>
+            `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
+              isActive
+                ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
+                : ""
+            }`
+          }
+        >
+          <FaBookReader
+            size={20}
+            className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
+          />
+          {!isCollapsed && <span>Learning & Development</span>}
+        </NavLink>
+
+        {/* Recruitment Link */}
+        <NavLink
+          to="#"
+          onClick={() => setIsRecruitmentOpen(!isRecruitmentOpen)}
+          className={`flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
+            isRecruitmentActive
+              ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
+              : ""
+          }`}
+        >
+          <FaCheckDouble
+            size={20}
+            className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
+          />
+          {!isCollapsed && <span>Recruitment</span>}
+        </NavLink>
+
+        {/* Offers and Onboarding Submenu */}
+        {isRecruitmentOpen && !isCollapsed && (
+          <div className="w-full pl-10 space-y-5">
             <NavLink
-              to="/learn"
+              to="/offers"
               className={({ isActive }) =>
                 `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
                   isActive
@@ -170,61 +178,35 @@ const Sidebar = () => {
                 }`
               }
             >
-              <FaBookReader
+              <FaFire
                 size={20}
                 className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
               />
-              {!isCollapsed && <span>Learning & Development</span>}
+              {!isCollapsed && <span>Offers</span>}
             </NavLink>
 
-            {/* Recruitment */}
             <NavLink
-              to="#"
-              onClick={() => setIsRecruitmentOpen(!isRecruitmentOpen)}
-              className="flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors"
+              to="/onboarding"
+              className={({ isActive }) =>
+                `flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors ${
+                  isActive
+                    ? "bg-gradient-to-r from-orange-400 to-orange-500 text-white"
+                    : ""
+                }`
+              }
             >
-              <FaCheckDouble
+              <FaBook
                 size={20}
                 className={`mr-2 ${isCollapsed ? "mx-auto" : ""}`}
               />
-              {!isCollapsed && <span>Recruitment</span>}
+              {!isCollapsed && <span>Onboarding</span>}
             </NavLink>
-
-            {/* Offers and Onboarding Submenu */}
-            {isRecruitmentOpen && !isCollapsed && (
-              <div className="ml-8">
-                <NavLink
-                  to="/offers"
-                  className="flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors"
-                >
-                  <FaGripfire size={20} className="mr-2" />
-                  <span>Offers</span>
-                </NavLink>
-
-                <NavLink
-                  to="/onboarding"
-                  className="flex items-center p-3 text-gray-600 hover:bg-orange-100 w-full rounded-r-[30px] transition-colors"
-                >
-                  <FaBook size={20} className="mr-2" />
-                  <span>Onboarding</span>
-                </NavLink>
-              </div>
-            )}
-          </>
+          </div>
         )}
 
-        {/* Render Sidebars based on the role */}
         {role === "Team Leader" && (
           <TeamLeaderSidebar isCollapsed={isCollapsed} />
         )}
-        {role === "Mid Lvl Manager" && (
-          <MidLvlManagersidebar isCollapsed={isCollapsed} />
-        )}
-        {role === "Top Lvl Manager" && (
-          <TopLvlManagersidebar isCollapsed={isCollapsed} />
-        )}
-        {role === "Ceo" && <CeoSidebar isCollapsed={isCollapsed} />}
-        {role === "HR" && <HRsidebar isCollapsed={isCollapsed} />}
       </div>
     </div>
   );
